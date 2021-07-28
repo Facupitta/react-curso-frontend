cd668b1b9097b59ca50c8711fe22f9b462a9e4ae
Facundo Pittaluga <facupitta@hotmail.com>
2021-07-28 19:32:14 -0300

    Header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return  <header className="Header">
+                <div className="Start">
+                    <p>ESTART BÚTSTRAP</p>
+                </div>
+                <div className="Menu">
+                    <p>Chinchulin</p>
+                    <p>Chinchulon</p>
+                    <p>Chinchulancha</p>
+                </div>
+            </header>
+}
+
+export default Header
