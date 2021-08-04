1d87f40e4ea09bd8849d435e4e95e190fd894721
Facundo Pittaluga <facupitta@hotmail.com>
2021-08-04 18:42:38 -0300

    Agregada la biblioteca de react-bootstrap como dependencia + código de prueba

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 //Components
 import Header from './components/Header'
 import Presentacion from './components/Presentacion'
@@ -5,6 +7,10 @@ import Portfolio from './components/Portfolio'
 import About from './components/About'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+
+import Container  from 'react-bootstrap/Container'
+import Row  from 'react-bootstrap/Row'
+import Col  from 'react-bootstrap/Col'
 
 //Styles
 import './styles/Header.css'
@@ -14,16 +20,22 @@ import './styles/About.css'
 import './styles/Contact.css'
 import './styles/Footer.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <Presentacion></Presentacion>
-      <Portfolio></Portfolio>
-      <About></About>
-      <Contact></Contact>
-      <Footer></Footer>
-    </div>
+    <Container>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={5}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 }
 
